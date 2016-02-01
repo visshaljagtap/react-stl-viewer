ba8256b6402893d2c39212647e261861c32f56c3
Chiedo <chiedo@chie.do>
2016-02-01 03:27:41 -0500

    Dev/testing environment

--- react/containers/App.js
+++ react/containers/App.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
 import STLViewer from '../../src/STLViewer';
+import qs from 'query-string';
 
 class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    let query = qs.parse(location.search);
+
+    this.state = {
+      url: query.url || 'http://localhost:4001/static/bottle.stl',
+      xDims:  parseInt(query.xDims, 10) || 53,
+      yDims:  parseInt(query.yDims, 10) || 85,
+      zDims:  parseInt(query.zDims, 10) || 27,
+      width:  parseInt(query.width, 10) || 400,
+      height: parseInt(query.height, 10) || 400,
+    };
+  }
+
   render(){
+    const { url, xDims, yDims, zDims, width, height } = this.state;
+
     return (
-      <STLViewer
-        url=""
-        xDims={150}
-        yDims={107}
-        zDims={79}
-        width={150}
-        height={150}
-        modelColor={0x005B83}
-        backgroundColor={0xEAEAEA}
-      />
+      <div>
+        <STLViewer
+          url={url}
+          xDims={xDims}
+          yDims={yDims}
+          zDims={zDims}
+          width={width}
+          height={height}
+          modelColor={0x005B83}
+          backgroundColor={0xEAEAEA}
+        />
+        <div>You can modify with following query strings: url, xDims, yDims, zDims, width, height.</div>
+      </div>
     );
   }
 };
